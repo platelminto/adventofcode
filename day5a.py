ece4d6664dd03487a75e11f3c626055195d7c426
Giorgio Momigliano <gmomigliano@protonmail.com>
2022-12-07 16:58:53 +0100

    cleaner? day5

--- day5a.py
+++ day5a.py
@@ -3,7 +3,7 @@ import re
 with open("input5.txt") as f:
     text = f.readlines()[10:]
 
-inital_state = [
+state = [
     '',
     'sphvfg',
     'mzdvbfjg',
@@ -16,7 +16,7 @@ inital_state = [
     'jqvpglf',
 ]
 
-state = [list(x.upper()) for x in inital_state]
+state = [list(col) for col in state]
 
 for instr in text:
     move_n, from_col, to_col = re.findall(r'\d+', instr)
@@ -24,4 +24,4 @@ for instr in text:
     for _ in range(int(move_n)):
         state[int(to_col)].insert(0, state[int(from_col)].pop(0))
 
-print(''.join([col[0] for col in state[1:]]))
+print(''.join([col[0] for col in state[1:]]))  # FCVRLMVQP
--- day5av2.py
+++ day5av2.py
@@ -16,11 +16,13 @@ state = [
     'jqvpglf',
 ]
 
+state = [list(col) for col in state]
+
 for instr in text:
     move_n, from_col, to_col = re.findall(r'\d+', instr)
     move_n, from_col, to_col = int(move_n), int(from_col), int(to_col)
 
-    state[to_col] = state[from_col][:move_n] + state[to_col]
+    state[to_col] = list(reversed(state[from_col][:move_n])) + state[to_col]
     state[from_col] = state[from_col][move_n:]
 
-print(''.join([col[0] for col in state[1:]]).upper())  # RWLWGJGFD
+print(''.join([col[0] for col in state[1:]]).upper())  # FCVRLMVQP
